7c967525d5e00c94f0b5579e8c7499c901590ab3
Krishna Chaitanya <DatlaKrishnaChaitanya.Varma@mrcooper.com>
2020-10-23 11:42:29 +0530

    Update search limit

--- src/javascripts/api/search.js
+++ src/javascripts/api/search.js
@@ -1,4 +1,4 @@
 export const getSearchResults = async (query, page) => {
- return fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&limit=30&page=${page}`)
+ return fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&limit=6&page=${page}`)
    .then(res => res.json());
 };
